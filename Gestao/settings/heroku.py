3df0bd7323d2580f09a31b85a0108586e375066f
rafa <arkerafa@gmail.com>
2021-10-05 14:44:11 -0300

    modeficando heroku.py

--- Gestao/settings/heroku.py
+++ Gestao/settings/heroku.py
@@ -1,26 +1,15 @@
-"""
-Production Settings for Heroku
-"""
-
 import environ
 
 from Gestao.settings.base import *
 
-env = environ.Env(
-    # set casting, default value
-    DEBUG=(bool, False)
-)
+env = environ.Env()
 
-# False if not in os.environ
-DEBUG = env('DEBUG')
+DEBUG = env.bool("DEBUG", False)
 
-# Raises django's ImproperlyConfigured exception if SECRET_KEY not in os.environ
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = env("SECRET_KEY")
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
 
-# Parse database connection url strings like psql://user:pass@127.0.0.1:8458/db
 DATABASES = {
-    # read os.environ['DATABASE_URL'] and raises ImproperlyConfigured exception if not found
-    'default': env.db(),
+    "default": env.db(),
 }
