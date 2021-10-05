f67cac5fe5aad1b9c704b56d24ccfa271c4e57b3
rafa <arkerafa@gmail.com>
2021-10-05 19:33:05 -0300

    teste view funcionario4

--- apps/website/views.py
+++ apps/website/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 from django.contrib.auth.decorators import login_required
 from apps.funcionarios.models import Funcionario
 
 @login_required
 def home(request):
-    data = {}
+    """data = {}
     data['usuario'] = request.user
-    return render(request, 'website/index.html', data)
+    return render(request, 'website/index.html', data)"""""
+    return HttpResponse('olá')
