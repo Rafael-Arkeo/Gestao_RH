25efa794c5f2b91bb355b81f721bc86dc71ca23d
rafa <arkerafa@gmail.com>
2021-10-05 21:11:31 -0300

    dfdfdfdf

--- gestao/settings/base.py
+++ gestao/settings/base.py
@@ -147,6 +147,6 @@ LOGIN_REDIRECT_URL = 'home'
 
 LOGOUT_REDIRECT_URL = 'login'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
